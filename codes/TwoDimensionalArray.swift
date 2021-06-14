dbc0c1082eb443667aac182709063836cdd0fae3
Suruikame <shiso0913@gmail.com>
2021-06-14 21:05:53 +0900

    Add TwoDimensionalArray

---
+++ codes/TwoDimensionalArray.swift
@@ -0,0 +1,20 @@
+///固定長二次元配列
+struct TwoDimensionalArray<Element> {
+    var elements: [Element]
+    var count: (Int, Int)
+    init(repeating initialValue: Element, count: (Int, Int)) {
+        self.elements = Array(repeating: initialValue, count: count.0 * count.1)
+        self.count = count
+    }
+    init(_ initialValue: Element, _ count: (Int, Int)) {
+        self.init(repeating: initialValue, count: count)
+    }
+    subscript(i: Int, j: Int) -> Element {
+        get {
+            return elements[self.count.1 * i + j]
+        }
+        set {
+            elements[self.count.1 * i + j] = newValue
+        }
+    }
+}
